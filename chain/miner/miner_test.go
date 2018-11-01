48893212c88db7f3bae1a71ce166fbbfa53e8664
paul_xie <xiegaoqiang0413@163.com>
2018-11-01 17:45:28 +0800

    refactor(miner):add unit test

--- chain/miner/miner_test.go
+++ chain/miner/miner_test.go
@@ -1,16 +1,230 @@
 package miner
 
 import (
+	"bytes"
+	"fmt"
 	"github.com/LemoFoundationLtd/lemochain-go/chain"
 	"github.com/LemoFoundationLtd/lemochain-go/chain/account"
 	"github.com/LemoFoundationLtd/lemochain-go/chain/deputynode"
+	"github.com/LemoFoundationLtd/lemochain-go/chain/params"
 	"github.com/LemoFoundationLtd/lemochain-go/chain/types"
 	"github.com/LemoFoundationLtd/lemochain-go/common"
 	"github.com/LemoFoundationLtd/lemochain-go/common/crypto"
 	"github.com/LemoFoundationLtd/lemochain-go/store"
+	"github.com/LemoFoundationLtd/lemochain-go/store/protocol"
 	"github.com/stretchr/testify/assert"
+	"math/big"
 	"testing"
+	"time"
 )
+
+type NodePair struct {
+	privateKey  string
+	publicKey   string
+	address     string
+	lemoAddress string
+}
+
+var Nodes = []*NodePair{
+	&NodePair{
+		privateKey:  "c21b6b2fbf230f665b936194d14da67187732bf9d28768aef1a3cbb26608f8aa",
+		publicKey:   "5e3600755f9b512a65603b38e30885c98cbac70259c3235c9b3f42ee563b480edea351ba0ff5748a638fe0aeff5d845bf37a3b437831871b48fd32f33cd9a3c0",
+		address:     "0x015780F8456F9c1532645087a19DcF9a7e0c7F97",
+		lemoAddress: "Lemo83GN72GYH2NZ8BA729Z9TCT7KQ5FC3CR6DJG",
+	},
+	&NodePair{
+		privateKey:  "9c3c4a327ce214f0a1bf9cfa756fbf74f1c7322399ffff925efd8c15c49953eb",
+		publicKey:   "ddb5fc36c415799e4c0cf7046ddde04aad6de8395d777db4f46ebdf258e55ee1d698fdd6f81a950f00b78bb0ea562e4f7de38cb0adf475c5026bb885ce74afb0",
+		address:     "016ad4Fc7e1608685Bf5fe5573973BF2B1Ef9B8A",
+		lemoAddress: "Lemo83JW7TBPA7P2P6AR9ZC2WCQJYRNHZ4NJD4CY",
+	},
+	&NodePair{
+		privateKey:  "ba9b51e59ec57d66b30b9b868c76d6f4d386ce148d9c6c1520360d92ef0f27ae",
+		publicKey:   "7739f34055d3c0808683dbd77a937f8e28f707d5b1e873bbe61f6f2d0347692f36ef736f342fb5ce4710f7e337f062cc2110d134b63a9575f78cb167bfae2f43",
+		address:     "01f98855Be9ecc5c23A28Ce345D2Cc04686f2c61",
+		lemoAddress: "Lemo842BJZ4DKCC764C63Y6A943775JH6NQ3Z33Y",
+	},
+	&NodePair{
+		privateKey:  "b381bad69ad4b200462a0cc08fcb8ba64d26efd4f49933c2c2448cb23f2cd9d0",
+		publicKey:   "34f0df789b46e9bc09f23d5315b951bc77bbfeda653ae6f5aab564c9b4619322fddb3b1f28d1c434250e9d4dd8f51aa8334573d7281e4d63baba913e9fa6908f",
+		address:     "0112fDDcF0C08132A5dcd9ED77e1a3348ff378D2",
+		lemoAddress: "Lemo837QGPS3YNTYNF53CD88WA5DR3ABNA95W2DG",
+	},
+	&NodePair{
+		privateKey:  "56b5fe1b8c40f0dec29b621a16ffcbc7a1bb5c0b0f910c5529f991273cd0569c",
+		publicKey:   "5b980ffb1b463fce4773a22ebf376c07c6207023b016b36ccfaba7be1cd1ab4a91737741cd43b7fcb10879e0fcf314d69fa953daec0f02be0f8f9cedb0cb3797",
+		address:     "016017aF50F4bB67101CE79298ACBdA1A3c12C15",
+		lemoAddress: "Lemo83HKZK68JQZDRGS5PWT2ZBSKR5CRADCSJB9B",
+	},
+	&NodePair{
+		privateKey:  "56b5fe1b8c40f0dec29b621a16ffcbc7a1bb5c0b0f910c5529f991273cd056ff",
+		publicKey:   "5b980ffb1b463fce4773a22ebf376c07c6207023b016b36ccfaba7be1cd1ab4a91737741cd43b7fcb10879e0fcf314d69fa953daec0f02be0f8f9cedb0cb37ff",
+		address:     "016017aF50F4bB67101CE79298ACBdA1A3c12Cff",
+		lemoAddress: "Lemo83HKZK68JQZDRGS5PWT2ZBSKR5CRADCSJBff",
+	},
+}
+
+type blockInfo struct {
+	hash        common.Hash
+	parentHash  common.Hash
+	height      uint32
+	author      common.Address
+	versionRoot common.Hash
+	txRoot      common.Hash
+	logsRoot    common.Hash
+	txList      []*types.Transaction
+	gasLimit    uint64
+	time        *big.Int
+	deputyRoot  []byte
+	deputyNodes deputynode.DeputyNodes
+}
+
+func makeBlock(db protocol.ChainDB, info blockInfo, save bool) *types.Block {
+	manager := account.NewManager(info.parentHash, db)
+	// sign transactions
+	var err error
+	var gasUsed uint64 = 0
+	txRoot := types.DeriveTxsSha(info.txList)
+	if txRoot != info.txRoot {
+		if info.txRoot != (common.Hash{}) {
+			fmt.Printf("%d txRoot hash error. except: %s, got: %s\n", info.height, info.txRoot.Hex(), txRoot.Hex())
+		}
+		info.txRoot = txRoot
+	}
+
+	var deputyRoot []byte
+	if len(info.deputyNodes) > 0 {
+		deputyRoot = types.DeriveDeputyRootSha(info.deputyNodes).Bytes()
+	}
+	if bytes.Compare(deputyRoot, info.deputyRoot) != 0 {
+		if len(info.deputyNodes) > 0 || len(info.deputyRoot) != 0 {
+			fmt.Printf("%d deputyRoot error. except: %s, got: %s\n", info.height, common.ToHex(info.deputyRoot), common.ToHex(deputyRoot))
+		}
+		info.deputyRoot = deputyRoot
+	}
+
+	// genesis coin
+	if info.height == 0 {
+		return nil
+	}
+	// account
+	salary := new(big.Int)
+	for _, tx := range info.txList {
+		gas := params.TxGas + params.TxDataNonZeroGas*uint64(len(tx.Data()))
+		fromAddr, err := tx.From()
+		if err != nil {
+			panic(err)
+		}
+		from := manager.GetAccount(fromAddr)
+		fee := new(big.Int).Mul(new(big.Int).SetUint64(gas), tx.GasPrice())
+		cost := new(big.Int).Add(tx.Amount(), fee)
+		to := manager.GetAccount(*tx.To())
+		// make sure the change log has right order
+		if fromAddr.Hex() < tx.To().Hex() {
+			from.SetBalance(new(big.Int).Sub(from.GetBalance(), cost))
+			to.SetBalance(new(big.Int).Add(to.GetBalance(), tx.Amount()))
+		} else {
+			to.SetBalance(new(big.Int).Add(to.GetBalance(), tx.Amount()))
+			from.SetBalance(new(big.Int).Sub(from.GetBalance(), cost))
+		}
+		gasUsed += gas
+		salary.Add(salary, fee)
+		from.(*account.SafeAccount).AppendTx(tx.Hash())
+		to.(*account.SafeAccount).AppendTx(tx.Hash())
+	}
+	if salary.Cmp(new(big.Int)) != 0 {
+		miner := manager.GetAccount(info.author)
+		miner.SetBalance(new(big.Int).Add(miner.GetBalance(), salary))
+	}
+	err = manager.Finalise()
+	if err != nil {
+		panic(err)
+	}
+	// header
+	if manager.GetVersionRoot() != info.versionRoot {
+		if info.versionRoot != (common.Hash{}) {
+			fmt.Printf("%d version root error. except: %s, got: %s\n", info.height, info.versionRoot.Hex(), manager.GetVersionRoot().Hex())
+		}
+		info.versionRoot = manager.GetVersionRoot()
+	}
+	changeLogs := manager.GetChangeLogs()
+	// fmt.Printf("%d changeLogs %v\n", info.height, changeLogs)
+	logsRoot := types.DeriveChangeLogsSha(changeLogs)
+	if logsRoot != info.logsRoot {
+		if info.logsRoot != (common.Hash{}) {
+			fmt.Printf("%d change logs root error. except: %s, got: %s\n", info.height, info.logsRoot.Hex(), logsRoot.Hex())
+		}
+		info.logsRoot = logsRoot
+	}
+	if info.time == nil {
+		info.time = new(big.Int).SetUint64(uint64(time.Now().Unix()))
+	}
+	if info.gasLimit == 0 {
+		info.gasLimit = 1000000
+	}
+	header := &types.Header{
+		ParentHash:  info.parentHash,
+		LemoBase:    info.author,
+		VersionRoot: info.versionRoot,
+		TxRoot:      info.txRoot,
+		LogsRoot:    info.logsRoot,
+		Bloom:       types.CreateBloom(nil),
+		EventRoot:   common.HexToHash("0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470"), // empty merkle
+		Height:      info.height,
+		GasLimit:    info.gasLimit,
+		GasUsed:     gasUsed,
+		Time:        info.time,
+		Extra:       []byte{},
+	}
+	if len(info.deputyRoot) > 0 {
+		header.DeputyRoot = make([]byte, len(info.deputyRoot))
+		copy(header.DeputyRoot[:], info.deputyRoot[:])
+	}
+
+	blockHash := header.Hash()
+	if blockHash != info.hash {
+		if info.hash != (common.Hash{}) {
+			fmt.Printf("%d block hash error. except: %s, got: %s\n", info.height, info.hash.Hex(), blockHash.Hex())
+		}
+		info.hash = blockHash
+	}
+	// block
+	block := &types.Block{
+		Txs:         info.txList,
+		ChangeLogs:  changeLogs,
+		DeputyNodes: info.deputyNodes,
+	}
+	block.SetHeader(header)
+	if save {
+		err = db.SetBlock(blockHash, block)
+		if err != nil && err != store.ErrExist {
+			panic(err)
+		}
+		err = manager.Save(blockHash)
+		if err != nil {
+			panic(err)
+		}
+	}
+	return block
+}
+
+func makeSignBlock(key string, db protocol.ChainDB, info blockInfo, save bool) (*types.Block, error) {
+	block := makeBlock(db, info, false)
+
+	tmp, err := crypto.HexToECDSA(key)
+	if err != nil {
+		return nil, err
+	}
+
+	hash := block.Hash()
+	signData, err := crypto.Sign(hash[:], tmp)
+	if err != nil {
+		return nil, err
+	}
+
+	block.Header.SignData = signData
+	return block, nil
+}
 
 type EngineTestForMiner struct{}
 
@@ -49,19 +263,18 @@ func newBlockChain() (*chain.BlockChain, chan *types.Block, error) {
 	blockChain.BroadcastStableBlock = broadcastStableBlock
 	blockChain.BroadcastConfirmInfo = broadcastConfirmInfo
 
-	deputynode.Instance().Add(0, chain.DefaultDeputyNodes)
 	return blockChain, ch, nil
+}
+
+func setSelfNodeKey(key string) {
+	tmp, _ := crypto.HexToECDSA(key)
+	deputynode.SetSelfNodeKey(tmp)
 }
 
 func newMiner(key string) (*Miner, chan *types.Block, chan *types.Block, error) {
 	store.ClearData()
 
-	tmp, err := crypto.HexToECDSA(key)
-	if err != nil {
-		return nil, nil, nil, err
-	} else {
-		deputynode.SetSelfNodeKey(tmp)
-	}
+	setSelfNodeKey(key)
 
 	blockChain, _, err := newBlockChain()
 	if err != nil {
@@ -80,13 +293,128 @@ func newMiner(key string) (*Miner, chan *types.Block, chan *types.Block, error) 
 		new(EngineTestForMiner)), mineNewBlockCh, recvBlockCh, nil
 }
 
-func TestMiner_modifyTimer(t *testing.T) {
-	store.ClearData()
+func calDeviation(ex int, src int) bool {
+	if ex == 0 {
+		if src == 0 {
+			return true
+		} else {
+			return false
+		}
+	} else {
+		if (ex >= src-1000) && (ex <= src+1000) {
+			return true
+		} else {
+			return false
+		}
+	}
+}
 
-	me := "c21b6b2fbf230f665b936194d14da67187732bf9d28768aef1a3cbb26608f8aa"
+func TestMiner_GetSleepGenesis(t *testing.T) {
+	store.ClearData()
+	deputynode.Instance().Add(0, chain.DefaultDeputyNodes)
+
+	me := Nodes[0].privateKey
 	miner, _, _, err := newMiner(me)
+	assert.NoError(t, err)
+	reset0 := miner.getSleepTime()
+
+	setSelfNodeKey(Nodes[1].privateKey)
+	reset1 := miner.getSleepTime()
+
+	setSelfNodeKey(Nodes[2].privateKey)
+	reset2 := miner.getSleepTime()
+
+	setSelfNodeKey(Nodes[3].privateKey)
+	reset3 := miner.getSleepTime()
+
+	setSelfNodeKey(Nodes[4].privateKey)
+	reset4 := miner.getSleepTime()
+
+	if (reset0 == 0) || (reset1 == 0) || (reset2 == 0) || (reset3 == 0) || (reset4 == 0) {
+		assert.Equal(t, true, true)
+	} else {
+		assert.Equal(t, true, false)
+	}
+}
+
+func TestMine_GetSleepNotSelf(t *testing.T) {
+	store.ClearData()
+	deputynode.Instance().Add(0, chain.DefaultDeputyNodes)
+
+	miner, _, _, err := newMiner(Nodes[0].privateKey)
+	assert.NoError(t, err)
+
+	genesis := miner.chain.GetBlockByHeight(0)
+	info := blockInfo{parentHash: genesis.Hash(), height: 1, author: common.HexToAddress(Nodes[0].address)}
+	block, err := makeSignBlock(Nodes[0].privateKey, miner.chain.Db(), info, false)
+	assert.NoError(t, err)
+
+	err = miner.chain.InsertChain(block, true)
+	assert.NoError(t, err)
+	miner.chain.SetStableBlock(block.Hash(), block.Height(), false)
+	assert.NoError(t, err)
+
+	setSelfNodeKey(Nodes[5].privateKey)
+	reset := miner.getSleepTime()
+	assert.Equal(t, -1, reset)
+}
+
+func TestMiner_TestMine_GetSleep1Deputy(t *testing.T) {
+	store.ClearData()
+	deputynode.Instance().Add(0, deputynode.DeputyNodes{chain.DefaultDeputyNodes[0]})
+	setSelfNodeKey(Nodes[0].privateKey)
+
+	miner, _, _, err := newMiner(Nodes[0].privateKey)
+	assert.NoError(t, err)
+
+	genesis := miner.chain.GetBlockByHeight(0)
+	info := blockInfo{parentHash: genesis.Hash(), height: 1, author: common.HexToAddress(Nodes[0].address)}
+	block, err := makeSignBlock(Nodes[0].privateKey, miner.chain.Db(), info, false)
+	assert.NoError(t, err)
+
+	err = miner.chain.InsertChain(block, true)
+	assert.NoError(t, err)
+	miner.chain.SetStableBlock(block.Hash(), block.Height(), false)
 	assert.NoError(t, err)
 
 	reset := miner.getSleepTime()
-	assert.Equal(t, 0, reset)
+	assert.Equal(t, 3000, reset)
+}
+
+func TestMiner_GetSleep(t *testing.T) {
+	store.ClearData()
+	deputynode.Instance().Add(0, chain.DefaultDeputyNodes)
+
+	me := Nodes[0].privateKey
+	miner, _, _, err := newMiner(me)
+	assert.NoError(t, err)
+
+	genesis := miner.chain.GetBlockByHeight(0)
+	info := blockInfo{parentHash: genesis.Hash(), height: 1, author: common.HexToAddress(Nodes[0].address)}
+	block, err := makeSignBlock(Nodes[0].privateKey, miner.chain.Db(), info, false)
+	assert.NoError(t, err)
+
+	err = miner.chain.InsertChain(block, true)
+	assert.NoError(t, err)
+	miner.chain.SetStableBlock(block.Hash(), block.Height(), false)
+	assert.NoError(t, err)
+
+	reset := miner.getSleepTime()
+	assert.Equal(t, calDeviation(40000, reset), true)
+
+	setSelfNodeKey(Nodes[1].privateKey)
+	reset = miner.getSleepTime()
+	assert.Equal(t, calDeviation(3000, reset), true)
+
+	setSelfNodeKey(Nodes[2].privateKey)
+	reset = miner.getSleepTime()
+	assert.Equal(t, calDeviation(10000, reset), true)
+
+	setSelfNodeKey(Nodes[3].privateKey)
+	reset = miner.getSleepTime()
+	assert.Equal(t, calDeviation(20000, reset), true)
+
+	setSelfNodeKey(Nodes[4].privateKey)
+	reset = miner.getSleepTime()
+	assert.Equal(t, calDeviation(30000, reset), true)
 }
