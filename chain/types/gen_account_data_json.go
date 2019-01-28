b76b4fc2d8bfd93bd1a6cf54228eb7cca249abf0
lnk <wyy102489@outlook.com>
2019-01-28 17:43:38 +0800

    feat(sdk): encode candidate information to json

--- chain/types/gen_account_data_json.go
+++ chain/types/gen_account_data_json.go
@@ -21,7 +21,9 @@ func (a AccountData) MarshalJSON() ([]byte, error) {
 		CodeHash      common.Hash                     `json:"codeHash" gencodec:"required"`
 		StorageRoot   common.Hash                     `json:"root" gencodec:"required"`
 		NewestRecords map[ChangeLogType]VersionRecord `json:"records" gencodec:"required"`
-		TxHashList    []common.Hash                   `json:"-"`
+		VoteFor       common.Address                  `json:"voteFor"`
+		Candidate     Candidate                       `json:"candidate"`
+		TxCount       int
 	}
 	var enc AccountData
 	enc.Address = a.Address
@@ -29,6 +31,9 @@ func (a AccountData) MarshalJSON() ([]byte, error) {
 	enc.CodeHash = a.CodeHash
 	enc.StorageRoot = a.StorageRoot
 	enc.NewestRecords = a.NewestRecords
+	enc.VoteFor = a.VoteFor
+	enc.Candidate = a.Candidate
+	enc.TxCount = a.TxCount
 	return json.Marshal(&enc)
 }
 
@@ -40,7 +45,9 @@ func (a *AccountData) UnmarshalJSON(input []byte) error {
 		CodeHash      *common.Hash                    `json:"codeHash" gencodec:"required"`
 		StorageRoot   *common.Hash                    `json:"root" gencodec:"required"`
 		NewestRecords map[ChangeLogType]VersionRecord `json:"records" gencodec:"required"`
-		TxHashList    []common.Hash                   `json:"-"`
+		VoteFor       *common.Address                 `json:"voteFor"`
+		Candidate     *Candidate                      `json:"candidate"`
+		TxCount       *int
 	}
 	var dec AccountData
 	if err := json.Unmarshal(input, &dec); err != nil {
@@ -66,5 +73,14 @@ func (a *AccountData) UnmarshalJSON(input []byte) error {
 		return errors.New("missing required field 'records' for AccountData")
 	}
 	a.NewestRecords = dec.NewestRecords
+	if dec.VoteFor != nil {
+		a.VoteFor = *dec.VoteFor
+	}
+	if dec.Candidate != nil {
+		a.Candidate = *dec.Candidate
+	}
+	if dec.TxCount != nil {
+		a.TxCount = *dec.TxCount
+	}
 	return nil
 }
