8f856ab18a9806f5e5dcd898b104e593abc71e00
paul_xie <xiegaoqiang0413@163.com>
2019-01-30 13:59:35 +0800

    refactor(store): add votes module

--- store/protocol/types.go
+++ store/protocol/types.go
@@ -33,7 +33,7 @@ type ChainDB interface {
 
 	CandidatesRanking(hash common.Hash)
 	GetCandidatesTop(hash common.Hash) []*store.Candidate
-	GetCandidatesPage(index int, size int) ([]common.Address, int, error)
+	GetCandidatesPage(index int, size int) ([]common.Address, uint32, error)
 
 	Close() error
 }
