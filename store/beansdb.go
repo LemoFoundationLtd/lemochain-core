2690c5a2b1edaebcc3c107a6596cd3d9a6c265ca
paul_xie <xiegaoqiang0413@163.com>
2019-01-31 14:59:26 +0800

    refactor(store): add votes module

--- store/beansdb.go
+++ store/beansdb.go
@@ -277,8 +277,8 @@ func (beansdb *BeansDB) Get(key []byte) ([]byte, error) {
 			return nil, nil
 		}
 
-		str := common.BytesToHash(route).Hex()
-		log.Error("str:" + str)
+		// str := common.BytesToHash(route).Hex()
+		// log.Error("str:" + str)
 
 		bitcask := beansdb.route(route)
 		val, err := bitcask.Get(uint(flg), route, key, offset)
