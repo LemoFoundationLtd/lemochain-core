7956f8a58d295d15d225213da4a7437bfc517210
paul_xie <xiegaoqiang0413@163.com>
2019-02-28 10:19:58 +0800

    fix(store): add asset module

---
+++ chain/types/asset.go
@@ -0,0 +1,66 @@
+package types
+
+import (
+	"github.com/LemoFoundationLtd/lemochain-go/common"
+	"math/big"
+)
+
+type Asset struct {
+	Category    int
+	IsDivisible bool
+	AssetCode   common.Hash
+	Decimals    int
+	TotalSupply *big.Int
+	IsMineable  bool
+	Issuer      common.Address
+	Profile     Profile
+}
+
+// type AssetExtend struct {
+// 	MateData  map[common.Hash]string
+// }
+
+type AssetEquity struct {
+	AssetCode common.Hash
+	AssetId   common.Hash
+	Equity    *big.Int
+}
+
+func (equity *AssetEquity) Clone() *AssetEquity {
+	return &AssetEquity{
+		AssetCode: equity.AssetCode,
+		AssetId:   equity.AssetId,
+		Equity:    new(big.Int).Set(equity.Equity),
+	}
+}
+
+func (asset *Asset) Clone() *Asset {
+
+	clone := func(profile Profile) Profile {
+		result := make(Profile)
+		if len(profile) <= 0 {
+			return result
+		} else {
+			for k, v := range profile {
+				result[k] = v
+			}
+
+			return result
+		}
+	}
+
+	if asset == nil {
+		return nil
+	} else {
+		return &Asset{
+			Category:    asset.Category,
+			IsDivisible: asset.IsDivisible,
+			AssetCode:   asset.AssetCode,
+			Decimals:    asset.Decimals,
+			TotalSupply: new(big.Int).Set(asset.TotalSupply),
+			IsMineable:  asset.IsMineable,
+			Issuer:      asset.Issuer,
+			Profile:     clone(asset.Profile),
+		}
+	}
+}
