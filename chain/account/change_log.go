0a97ad0b3911cd3090d22eb95b63c5b8251c24a7
paul_xie <xiegaoqiang0413@163.com>
2019-03-06 11:40:10 +0800

    fix(store): add asset module

--- chain/account/change_log.go
+++ chain/account/change_log.go
@@ -158,10 +158,12 @@ func decodeEvent(s *rlp.Stream) (interface{}, error) {
 
 func decodeCandidate(s *rlp.Stream) (interface{}, error) {
 	_, size, _ := s.Kind()
-	result := make(types.Profile)
 	if size <= 0 {
-		return &result, nil
+		var result interface{}
+		err := s.Decode(&result)
+		return &result, err
 	} else {
+		result := make(types.Profile)
 		err := s.Decode(&result)
 		return &result, err
 	}
@@ -188,7 +190,9 @@ func decodeString(s *rlp.Stream) (interface{}, error) {
 func decodeAsset(s *rlp.Stream) (interface{}, error) {
 	_, size, _ := s.Kind()
 	if size <= 0 {
-		return nil, nil
+		var result interface{}
+		err := s.Decode(&result)
+		return nil, err
 	} else {
 		var result types.Asset
 		result.TotalSupply = new(big.Int)
@@ -199,23 +203,25 @@ func decodeAsset(s *rlp.Stream) (interface{}, error) {
 }
 
 func decodeEquity(s *rlp.Stream) (interface{}, error) {
-	// _, size, _ := s.Kind()
-	// if size <= 0 {
-	// 	return nil, nil
-	// }else{
-	var result types.AssetEquity
-	result.Equity = new(big.Int)
-	// err := s.Decode(&result)
-	// return &result, err
-	s.Decode(&result)
-	return &result, nil
-	// }
+	_, size, _ := s.Kind()
+	if size <= 0 {
+		var result interface{}
+		err := s.Decode(&result)
+		return nil, err
+	} else {
+		var result types.AssetEquity
+		result.Equity = new(big.Int)
+		err := s.Decode(&result)
+		return &result, err
+	}
 }
 
 func decodeProfileChangeLogExtra(s *rlp.Stream) (interface{}, error) {
 	_, size, _ := s.Kind()
 	if size <= 0 {
-		return nil, nil
+		var result interface{}
+		err := s.Decode(&result)
+		return nil, err
 	} else {
 		var result ProfileChangeLogExtra
 		err := s.Decode(&result)
@@ -770,16 +776,14 @@ func NewEquityLog(processor types.ChangeLogProcessor, account types.AccountAcces
 		Extra:   id,
 	}
 
-	emptyEquity := new(types.AssetEquity)
-	emptyEquity.Equity = new(big.Int)
 	if oldValue == nil {
-		log.OldVal = emptyEquity
+		log.OldVal = nil
 	} else {
 		log.OldVal = oldValue.Clone()
 	}
 
 	if newVal == nil {
-		log.NewVal = emptyEquity
+		log.NewVal = nil
 	} else {
 		log.NewVal = newVal.Clone()
 	}
