c7a56567c33c6efc6f6d005e030877d1d2d76b5e
sandy <18382255942@163.com>
2019-03-16 10:32:18 +0800

    fix(tx_processor_test): fix some tx test

--- chain/tx_processor_test.go
+++ chain/tx_processor_test.go
@@ -57,7 +57,6 @@ func TestTxProcessor_Process(t *testing.T) {
 	block = defaultBlocks[3]
 	t.Log(block)
 	gasUsed, err = p.Process(block.Header, block.Txs)
-	newHeader, err = p.Process(block)
 	assert.NoError(t, err)
 	assert.Equal(t, block.Header.GasUsed, gasUsed)
 	assert.Equal(t, block.Header.VersionRoot, p.am.GetVersionRoot())
