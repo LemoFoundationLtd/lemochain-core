e8cb7d733a1f1a40390488067073d11aabaed068
lnk <wyy102489@outlook.com>
2019-06-14 23:15:13 +0800

    feat(chain): ready to release 1.3.0

--- chain/params/version.go
+++ chain/params/version.go
@@ -4,8 +4,8 @@ import "fmt"
 
 const (
 	VersionMajor = 1 // Major version component of the current release
-	VersionMinor = 1 // Minor version component of the current release
-	VersionPatch = 2 // Patch version component of the current release
+	VersionMinor = 3 // Minor version component of the current release
+	VersionPatch = 0 // Patch version component of the current release
 )
 
 var Version = func() string {
