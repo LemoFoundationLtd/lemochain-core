449c96da642f7892e81b0c904e87959e95546b59
paul_xie <xiegaoqiang0413@163.com>
2019-06-14 10:13:59 +0800

    refactor(tx_pool):refactor tx pool interface

--- network/chain_interface.go
+++ network/chain_interface.go
@@ -29,8 +29,21 @@ type BlockChain interface {
 }
 
 type TxPool interface {
-	// AddTxs add transaction
-	RecvTxs(tx []*types.Transaction)
-	// Remove remove transaction
+	/* 本节点出块时，从交易池中取出交易进行打包，但并不从交易池中删除 */
+	Get(time uint32, size int) []*types.Transaction
+
+	/* 本节点出块时，执行交易后，发现错误的交易通过该接口进行删除 */
+	DelInvalidTxs(txs []*types.Transaction)
+
+	/* 新收一个块时，验证块中的交易是否被同一条分叉上的其他块打包了 */
+	VerifyTxInBlock(block *types.Block) bool
+
+	/* 新收到一个通过验证的新块（包括本节点出的块），需要从交易池中删除该块中已打包的交易 */
 	RecvBlock(block *types.Block)
+
+	/* 收到一笔新的交易 */
+	RecvTx(tx *types.Transaction) bool
+
+	/* 对链进行剪枝，剪下的块中的交易需要回归交易池 */
+	PruneBlock(block *types.Block)
 }
