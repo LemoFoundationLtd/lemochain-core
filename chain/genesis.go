54a43446c35c02230fb806b3be62fe1ccaf43871
sandy <18382255942@163.com>
2019-06-19 10:48:30 +0800

    fix(chain): fix pr

--- chain/genesis.go
+++ chain/genesis.go
@@ -33,78 +33,61 @@ type candidateInfo struct {
 }
 
 var (
-	DefaultFounder     = decodeMinerAddress("Lemo83GN72GYH2NZ8BA729Z9TCT7KQ5FC3CR6DJG") // Initial LEMO holder
-	DefaultDeputyNodes = deputynode.DeputyNodes{
-		&deputynode.DeputyNode{
-			MinerAddress: DefaultFounder,
-			NodeID:       common.FromHex("0x5e3600755f9b512a65603b38e30885c98cbac70259c3235c9b3f42ee563b480edea351ba0ff5748a638fe0aeff5d845bf37a3b437831871b48fd32f33cd9a3c0"),
-			Rank:         0,
-			Votes:        new(big.Int).SetInt64(5),
-		},
-		&deputynode.DeputyNode{
-			MinerAddress: decodeMinerAddress("Lemo83JW7TBPA7P2P6AR9ZC2WCQJYRNHZ4NJD4CY"),
-			NodeID:       common.FromHex("0xddb5fc36c415799e4c0cf7046ddde04aad6de8395d777db4f46ebdf258e55ee1d698fdd6f81a950f00b78bb0ea562e4f7de38cb0adf475c5026bb885ce74afb0"),
-			Rank:         1,
-			Votes:        new(big.Int).SetInt64(4),
-		},
-		&deputynode.DeputyNode{
-			MinerAddress: decodeMinerAddress("Lemo842BJZ4DKCC764C63Y6A943775JH6NQ3Z33Y"),
-			NodeID:       common.FromHex("0x7739f34055d3c0808683dbd77a937f8e28f707d5b1e873bbe61f6f2d0347692f36ef736f342fb5ce4710f7e337f062cc2110d134b63a9575f78cb167bfae2f43"),
-			Rank:         2,
-			Votes:        new(big.Int).SetInt64(3),
-		},
-		&deputynode.DeputyNode{
-			MinerAddress: decodeMinerAddress("Lemo837QGPS3YNTYNF53CD88WA5DR3ABNA95W2DG"),
-			NodeID:       common.FromHex("0x34f0df789b46e9bc09f23d5315b951bc77bbfeda653ae6f5aab564c9b4619322fddb3b1f28d1c434250e9d4dd8f51aa8334573d7281e4d63baba913e9fa6908f"),
-			Rank:         3,
-			Votes:        new(big.Int).SetInt64(2),
-		},
-		&deputynode.DeputyNode{
-			MinerAddress: decodeMinerAddress("Lemo83HKZK68JQZDRGS5PWT2ZBSKR5CRADCSJB9B"),
-			NodeID:       common.FromHex("0x5b980ffb1b463fce4773a22ebf376c07c6207023b016b36ccfaba7be1cd1ab4a91737741cd43b7fcb10879e0fcf314d69fa953daec0f02be0f8f9cedb0cb3797"),
-			Rank:         4,
-			Votes:        new(big.Int).SetInt64(1),
-		},
-	}
-
+	DefaultFounder         = decodeMinerAddress("Lemo83GN72GYH2NZ8BA729Z9TCT7KQ5FC3CR6DJG") // Initial LEMO holder
+	DefaultDeputyNodes     = InitDeputyNodes()
 	DefaultDeputyNodesInfo = infos{
 		&candidateInfo{
-			MinerAddress:  DefaultDeputyNodes[0].MinerAddress,
-			IncomeAddress: DefaultDeputyNodes[0].MinerAddress,
-			NodeID:        DefaultDeputyNodes[0].NodeID,
-			Host:          "www.lemochain.com",
+			MinerAddress:  DefaultFounder,
+			IncomeAddress: decodeMinerAddress("Lemo83GN72GYH2NZ8BA729Z9TCT7KQ5FC3CR6DJG"),
+			NodeID:        common.FromHex("0x5e3600755f9b512a65603b38e30885c98cbac70259c3235c9b3f42ee563b480edea351ba0ff5748a638fe0aeff5d845bf37a3b437831871b48fd32f33cd9a3c0"),
+			Host:          "10.0.22.23",
 			Port:          "7001",
 		},
 		&candidateInfo{
-			MinerAddress:  DefaultDeputyNodes[1].MinerAddress,
-			IncomeAddress: DefaultDeputyNodes[1].MinerAddress,
-			NodeID:        DefaultDeputyNodes[1].NodeID,
-			Host:          "www.lemolabs.com",
+			MinerAddress:  decodeMinerAddress("Lemo83JW7TBPA7P2P6AR9ZC2WCQJYRNHZ4NJD4CY"),
+			IncomeAddress: decodeMinerAddress("Lemo83JW7TBPA7P2P6AR9ZC2WCQJYRNHZ4NJD4CY"),
+			NodeID:        common.FromHex("0xddb5fc36c415799e4c0cf7046ddde04aad6de8395d777db4f46ebdf258e55ee1d698fdd6f81a950f00b78bb0ea562e4f7de38cb0adf475c5026bb885ce74afb0"),
+			Host:          "10.0.22.23",
 			Port:          "7002",
 		},
 		&candidateInfo{
-			MinerAddress:  DefaultDeputyNodes[2].MinerAddress,
-			IncomeAddress: DefaultDeputyNodes[2].MinerAddress,
-			NodeID:        DefaultDeputyNodes[2].NodeID,
-			Host:          "10.0.22.24",
+			MinerAddress:  decodeMinerAddress("Lemo842BJZ4DKCC764C63Y6A943775JH6NQ3Z33Y"),
+			IncomeAddress: decodeMinerAddress("Lemo842BJZ4DKCC764C63Y6A943775JH6NQ3Z33Y"),
+			NodeID:        common.FromHex("0x7739f34055d3c0808683dbd77a937f8e28f707d5b1e873bbe61f6f2d0347692f36ef736f342fb5ce4710f7e337f062cc2110d134b63a9575f78cb167bfae2f43"),
+			Host:          "10.0.22.23",
 			Port:          "7003",
 		},
 		&candidateInfo{
-			MinerAddress:  DefaultDeputyNodes[3].MinerAddress,
-			IncomeAddress: DefaultDeputyNodes[3].MinerAddress,
-			NodeID:        DefaultDeputyNodes[3].NodeID,
-			Host:          "10.0.23.24",
+			MinerAddress:  decodeMinerAddress("Lemo837QGPS3YNTYNF53CD88WA5DR3ABNA95W2DG"),
+			IncomeAddress: decodeMinerAddress("Lemo837QGPS3YNTYNF53CD88WA5DR3ABNA95W2DG"),
+			NodeID:        common.FromHex("0x34f0df789b46e9bc09f23d5315b951bc77bbfeda653ae6f5aab564c9b4619322fddb3b1f28d1c434250e9d4dd8f51aa8334573d7281e4d63baba913e9fa6908f"),
+			Host:          "10.0.22.23",
 			Port:          "7004",
 		},
 		&candidateInfo{
-			MinerAddress:  DefaultDeputyNodes[4].MinerAddress,
-			IncomeAddress: DefaultDeputyNodes[4].MinerAddress,
-			NodeID:        DefaultDeputyNodes[4].NodeID,
-			Host:          "10.0.22.23",
+			MinerAddress:  decodeMinerAddress("Lemo83HKZK68JQZDRGS5PWT2ZBSKR5CRADCSJB9B"),
+			IncomeAddress: decodeMinerAddress("Lemo83HKZK68JQZDRGS5PWT2ZBSKR5CRADCSJB9B"),
+			NodeID:        common.FromHex("0x5b980ffb1b463fce4773a22ebf376c07c6207023b016b36ccfaba7be1cd1ab4a91737741cd43b7fcb10879e0fcf314d69fa953daec0f02be0f8f9cedb0cb3797"),
+			Host:          "10.0.22.24",
 			Port:          "7005",
 		},
 	}
 )
+
+// InitDeputyNodes 初始化第一届出块节点列表
+func InitDeputyNodes() deputynode.DeputyNodes {
+	deputyNodes := make(deputynode.DeputyNodes, 0)
+	for i, info := range DefaultDeputyNodesInfo {
+		node := &deputynode.DeputyNode{
+			MinerAddress: info.MinerAddress,
+			NodeID:       info.NodeID,
+			Rank:         uint32(i),
+			Votes:        new(big.Int).SetInt64(0),
+		}
+		deputyNodes = append(deputyNodes, node)
+	}
+	return deputyNodes
+}
 
 func decodeMinerAddress(input string) common.Address {
 	if address, err := common.StringToAddress(input); err == nil {
@@ -193,8 +176,11 @@ func SetupGenesisBlock(db protocol.ChainDB, genesis *Genesis) *types.Block {
 
 // ToBlock
 func (g *Genesis) ToBlock(am *account.Manager) (*types.Block, error) {
-	g.setInitalCandidatelistsInfo(am)
+	// set balance for some account
 	g.setBalance(am)
+	// register candidate node for first term deputy nodes
+	g.setInitalCandidatelistsInfo(am)
+
 	err := am.Finalise()
 	if err != nil {
 		return nil, err
@@ -232,6 +218,9 @@ func (g *Genesis) setInitalCandidatelistsInfo(am *account.Manager) {
 		profile[types.CandidateKeyNodeID] = common.ToHex(v.NodeID)
 		profile[types.CandidateKeyHost] = v.Host
 		profile[types.CandidateKeyPort] = v.Port
-		am.GetAccount(v.MinerAddress).SetCandidate(profile)
+		acc := am.GetAccount(v.MinerAddress)
+		acc.SetCandidate(profile)
+		acc.SetVoteFor(v.MinerAddress)
+		acc.SetVotes(acc.GetBalance())
 	}
 }
