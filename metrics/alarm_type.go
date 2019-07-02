739b7cceb460e0e2b09b31bf7e33bbfbdb77aa1d
sandy <18382255942@163.com>
2019-07-02 16:38:43 +0800

    fix(tx): fix candidate deposit pr

--- metrics/alarm_type.go
+++ metrics/alarm_type.go
@@ -5,6 +5,7 @@ type MapMetr map[string]interface{}
 
 // 验证触发告警条件
 type VerifyCondition struct {
-	AlarmReason    string
-	AlarmCondition float64
+	AlarmReason string
+	MetricsType string
+	AlarmValue  float64
 }
