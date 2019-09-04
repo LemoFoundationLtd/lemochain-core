17c171a835691f174dc79da46fef54f8462e301a
lnk <wyy102489@outlook.com>
2019-09-04 22:06:14 +0800

    refactor(consensus): simplify the stable and fork logic; add cache for block signature and the nodeID recovered by current block

---
+++ chain/consensus/block_signer_test.go
@@ -0,0 +1,48 @@
+package consensus
+
+import (
+	"crypto/ecdsa"
+	"crypto/rand"
+	"github.com/LemoFoundationLtd/lemochain-core/chain/deputynode"
+	"github.com/LemoFoundationLtd/lemochain-core/chain/types"
+	"github.com/LemoFoundationLtd/lemochain-core/common/crypto"
+	"github.com/LemoFoundationLtd/lemochain-core/common/crypto/secp256k1"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestSignBlock(t *testing.T) {
+	key, _ := ecdsa.GenerateKey(secp256k1.S256(), rand.Reader)
+	deputynode.SetSelfNodeKey(key)
+	block := types.Block{Header: &types.Header{}}
+	hash := block.Hash()
+
+	// sign and recover
+	sig, err := SignBlock(hash)
+	assert.NoError(t, err)
+	block.Header.SignData = sig
+	nodeID, err := block.SignerNodeID()
+	assert.NoError(t, err)
+	assert.Equal(t, crypto.PrivateKeyToNodeID(key), nodeID)
+
+	// sign another hash
+	block.Header.Height++
+	sig2, err := SignBlock(block.Hash())
+	assert.NoError(t, err)
+	assert.NotEqual(t, sig, sig2)
+}
+
+func BenchmarkSignBlock(b *testing.B) {
+	key, _ := ecdsa.GenerateKey(secp256k1.S256(), rand.Reader)
+	deputynode.SetSelfNodeKey(key)
+	block := types.Block{Header: &types.Header{}}
+	hash := block.Hash()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		for j := 0; j < 400; j++ {
+			_, err := SignBlock(hash)
+			assert.NoError(b, err)
+		}
+	}
+}
