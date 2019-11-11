61e412036cb7245d4bb47b7f178091724b572453
lnk <wyy102489@outlook.com>
2019-11-12 01:17:11 +0800

    fix(consensus): fix the stop mining bug when terms change

--- chain/consensus/schedule_test.go
+++ chain/consensus/schedule_test.go
@@ -211,7 +211,7 @@ func TestGetCorrectMiner_Error(t *testing.T) {
 	_, err := GetCorrectMiner(parent, int64(parent.Time-10)*1000, mineTimeout, dm)
 	assert.Equal(t, ErrSmallerMineTime, err)
 
-	parent = &types.Header{Time: uint32(time.Now().Unix()), MinerAddress: common.HexToAddress("0x123")}
+	parent = &types.Header{Time: uint32(time.Now().Unix()), Height: 1, MinerAddress: common.HexToAddress("0x123")}
 	_, err = GetCorrectMiner(parent, int64(parent.Time+10)*1000, mineTimeout, dm)
 	assert.Equal(t, ErrNotDeputy, err)
 }
@@ -284,6 +284,81 @@ func TestGetCorrectMiner(t *testing.T) {
 			miner, err := GetCorrectMiner(parent, parentBlockTime+test.timeDistance, mineTimeout, dm)
 			assert.NoError(t, err)
 			assert.Equal(t, testDeputies[test.wantMinerIndex].MinerAddress, miner)
+		})
+	}
+}
+
+func TestGetCorrectMiner_CrossTerm(t *testing.T) {
+	deputyCount := 3
+	dm := deputynode.NewManager(deputyCount, &testBlockLoader{})
+	dm.SaveSnapshot(0, testDeputies.ToDeputyNodes()[:deputyCount])
+	// different deputies in the second term
+	term2Deputies := generateDeputies(deputyCount)
+	dm.SaveSnapshot(params.TermDuration, term2Deputies.ToDeputyNodes())
+	type testInfo struct {
+		parentHeight uint32
+		// for the first term
+		parentMinerIndex int
+		timeDistance     int64
+		wantDeputy       deputyTestData
+	}
+
+	var blockInterval int64 = 1000
+	var mineTimeout int64 = 2000
+	oneLoopTime := mineTimeout * int64(deputyCount)
+	parentBlockTime := int64(time.Now().Unix()) * 1000
+	tests := []testInfo{
+		// mine normal block
+		{10, 0, 0, testDeputies[1]},
+		{10, 0, 10, testDeputies[1]},
+		{10, 0, blockInterval, testDeputies[1]},
+		{10, 0, mineTimeout * 1, testDeputies[2]},
+		{10, 0, mineTimeout * 2, testDeputies[0]},
+		{10, 0, oneLoopTime, testDeputies[1]},
+		{10, 1, 0, testDeputies[2]},
+		{10, 1, 10, testDeputies[2]},
+		{10, 1, blockInterval, testDeputies[2]},
+		{10, 1, mineTimeout * 1, testDeputies[0]},
+		{10, 1, mineTimeout * 2, testDeputies[1]},
+		{10, 1, oneLoopTime, testDeputies[2]},
+		{10, 2, 0, testDeputies[0]},
+		{10, 2, 10, testDeputies[0]},
+		{10, 2, blockInterval, testDeputies[0]},
+		{10, 2, mineTimeout * 1, testDeputies[1]},
+		{10, 2, mineTimeout * 2, testDeputies[2]},
+		{10, 2, oneLoopTime, testDeputies[0]},
+		// mine first block
+		{0, 0, 0, testDeputies[0]},
+		{0, 0, 10, testDeputies[0]},
+		{0, 0, blockInterval, testDeputies[0]},
+		{0, 0, mineTimeout * 1, testDeputies[1]},
+		{0, 0, mineTimeout * 2, testDeputies[2]},
+		{0, 0, mineTimeout * 3, testDeputies[0]},
+		{0, 1, 0, testDeputies[0]},
+		{0, 2, 0, testDeputies[0]},
+		// mine reward block
+		{params.TermDuration + params.InterimDuration, 0, 0, term2Deputies[0]},
+		{params.TermDuration + params.InterimDuration, 0, 10, term2Deputies[0]},
+		{params.TermDuration + params.InterimDuration, 0, blockInterval, term2Deputies[0]},
+		{params.TermDuration + params.InterimDuration, 0, mineTimeout * 1, term2Deputies[1]},
+		{params.TermDuration + params.InterimDuration, 0, mineTimeout * 2, term2Deputies[2]},
+		{params.TermDuration + params.InterimDuration, 0, mineTimeout * 3, term2Deputies[0]},
+		{params.TermDuration + params.InterimDuration, 1, 0, term2Deputies[0]},
+		{params.TermDuration + params.InterimDuration, 2, 0, term2Deputies[0]},
+	}
+	for _, test := range tests {
+		caseName := fmt.Sprintf("parentHeight=%d,parentMiner=%d,timeDistance=%d", test.parentHeight, test.parentMinerIndex, test.timeDistance)
+		t.Run(caseName, func(t *testing.T) {
+			test := test // capture range variable
+			t.Parallel()
+			parent := &types.Header{
+				Height:       test.parentHeight,
+				Time:         uint32(parentBlockTime / 1000),
+				MinerAddress: testDeputies[test.parentMinerIndex].MinerAddress,
+			}
+			miner, err := GetCorrectMiner(parent, parentBlockTime+test.timeDistance, mineTimeout, dm)
+			assert.NoError(t, err)
+			assert.Equal(t, test.wantDeputy.MinerAddress, miner)
 		})
 	}
 }
